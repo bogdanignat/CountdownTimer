d2dfa0def58aceaecf25b55fc1cd355c0422cd61
Bogdan Ignat <bogdan.ignat80@gmail.com>
2022-01-05 17:17:42 +0200

    modify some css, delete few extra lines from .js and added some comm

--- script.js
+++ script.js
@@ -3,39 +3,48 @@ const hoursDisplay = document.getElementById('hours_count_id')
 const minutesDisplay = document.getElementById('minutes_count_id')
 const secondsDisplay = document.getElementById('seconds_count_id')
 
-let newYears = '1 Jan 2023'
+let theDate = '1 Jan 2023'
+const newYearsDate = new Date(theDate)
+const currentDate = new Date()
+console.log(newYearsDate)
+console.log(currentDate)
 
 function countdown() {
-  const newYearsDate = new Date(newYears)
+  // get the curent date and the date that we def
+  const newYearsDate = new Date(theDate)
   const currentDate = new Date()
 
+  //the diference, in seconds
   const totalSeconds = (newYearsDate - currentDate) / 1000
 
+  //round it up and get the min, hours and days from totalSeconds
   const days = Math.floor(totalSeconds / 3600 / 24)
   const hours = Math.floor(totalSeconds / 3600) % 24
   const mins = Math.floor(totalSeconds / 60) % 60
   const seconds = Math.floor(totalSeconds) % 60
 
+  // modify the html
   daysDisplay.innerHTML = days
   hoursDisplay.innerHTML = formatTime(hours)
   minutesDisplay.innerHTML = formatTime(mins)
   secondsDisplay.innerHTML = formatTime(seconds)
 }
 
+//add a '0' in front of the seconds, when < 10
 function formatTime(time) {
   return time < 10 ? `0${time}` : time
-  // if time<0 then return the the string `0${time}`, if not, return time
+  // if time<10 then return `0${time}`, if not, return time
 }
 
+//call the function
 countdown()
+//call the function every second
 setInterval(countdown, 1000)
 
-document.getElementById('input_date').addEventListener('change', function () {
-  let input = this.value
-  console.log(input) // 2015-11-13
-  newYears = this.value
-  // var dateEntered = new Date(input)
-  // console.log(dateEntered) // Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+//modify the date for the end of countdown
+const inputDate = document.getElementById('input_date')
+inputDate.addEventListener('change', function () {
+  //get the date from input and = with theDate
+  theDate = this.value
+  console.log(theDate)
 })
-
-console.log(new Date())
